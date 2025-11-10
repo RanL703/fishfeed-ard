e8e8f9c1e32d8716107ff468855245056a03437b
RanL703 <laskar.ranadeep@gmail.com>
2025-11-10 22:59:03 +0530

    Init

---
+++ app/demo/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+import React from "react";
+import { HeroSection } from "@/components/ui/hero-odyssey";
+
+export default function DemoPage() {
+  return (
+    <div className="flex w-full h-screen justify-center items-center bg-black">
+      <HeroSection />
+    </div>
+  );
+}
